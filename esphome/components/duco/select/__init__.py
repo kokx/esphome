c2bafc1b9ea47df27fb6aa01bcc9d308e72df4ea
korneo51 <elucid.escape@gmail.com>
2026-05-29 21:15:53 +0200

    Add Duco bypass mode select

--- esphome/components/duco/select/__init__.py
+++ esphome/components/duco/select/__init__.py
@@ -25,23 +25,48 @@ DUCO_MODE_OPTIONS = [
     "MAN3x3",
 ]
 
+CONF_BYPASS_MODE = "bypass_mode"
+
+DUCO_BYPASS_MODE_OPTIONS = [
+    "AUTO",
+    "CLOSED",
+    "OPEN",
+]
+
 duco_ns = cg.esphome_ns.namespace("duco")
 DucoSelect = duco_ns.class_("DucoSelect", cg.PollingComponent, select.Select)
+DucoBypassModeSelect = duco_ns.class_(
+    "DucoBypassModeSelect", cg.PollingComponent, select.Select
+)
 
 CONFIG_SCHEMA = cv.All(
     select.select_schema(DucoSelect)
     .extend(cv.COMPONENT_SCHEMA)
     .extend(cv.polling_component_schema("3s"))
     .extend(DUCO_COMPONENT_SCHEMA)
-    .extend({cv.GenerateID(): cv.declare_id(DucoSelect)})
+    .extend(
+        {
+            cv.GenerateID(): cv.declare_id(DucoSelect),
+            cv.Optional(CONF_BYPASS_MODE): select.select_schema(DucoBypassModeSelect)
+            .extend(cv.COMPONENT_SCHEMA)
+            .extend(cv.polling_component_schema("60s"))
+            .extend({cv.GenerateID(): cv.declare_id(DucoBypassModeSelect)}),
+        }
+    )
 )
 
 
 async def to_code(config):
+    parent = await cg.get_variable(config[CONF_DUCO_ID])
+
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-
     await select.register_select(var, config, options=DUCO_MODE_OPTIONS)
-
-    parent = await cg.get_variable(config[CONF_DUCO_ID])
     cg.add(var.set_parent(parent))
+
+    if CONF_BYPASS_MODE in config:
+        bypass_mode_config = config[CONF_BYPASS_MODE]
+        var = cg.new_Pvariable(bypass_mode_config[CONF_ID])
+        await cg.register_component(var, bypass_mode_config)
+        await select.register_select(var, bypass_mode_config, options=DUCO_BYPASS_MODE_OPTIONS)
+        cg.add(var.set_parent(parent))
